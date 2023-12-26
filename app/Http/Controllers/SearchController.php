4b2562b08e4c6d2bf7f3c16de102350fd430267f
djurisicdanica <danaidanica@gmail.com>
2023-12-26 15:05:35 +0100

    filter i paginacija

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -8,10 +8,10 @@ use App\Models\RealEstate;
 class SearchController extends Controller
 {
     public function index(Request $request)
-{
+    {   
     $data['q'] = $request->query('q');
-    $data['selectedLocation'] = $request->query('location');
     $data['locations'] = RealEstate::distinct('location')->pluck('location')->toArray();
+    $data['selectedLocation'] = $request->query('location_name');
     $data['operator'] = $request->query('operator');
     $data['price_start'] = $request->query('price_start');
     $data['price_end'] = $request->query('price_end');
@@ -23,27 +23,29 @@ class SearchController extends Controller
         'izmedju' => 'izmedju',
     ];
 
-    $query = RealEstate::where(function ($query) use ($data) {
-        $query->where('username', 'like', '%' . $data['q']. '%');
-        $query->orWhere('description', 'like', '%' . $data['q']. '%');
+    $query = RealEstate::select();
+
+    $query->where(function ($query) use ($data) {
+        $query->where('username', 'like', '%' . $data['q']. '%')
+              ->orWhere('description', 'like', '%' . $data['q']. '%');
+    });
+
+    if ($data['selectedLocation']) {
+        $query->where('location', 'like', '%' . $data['selectedLocation'] . '%');
+    }
+
+    if ($data['operator']) {
+        if ($data['operator'] == 'izmedju') {
+            $query->whereBetween('price', [$data['price_start'], $data['price_end']]);
+        } else {
+            $query->where('price', $data['operator'], $data['price_start']);
+        }
+    }
+
     
-        if ($data['selectedLocation']) {
-            $query->where('location', 'like', '%' . $data['selectedLocation'] . '%');
-        }
-        if ($data['operator']) {
-            if ($data['operator'] == 'izmedju') {
-                $query->whereRaw('price BETWEEN ? AND ?', [$data['price_start'], $data['price_end']]);
-            } else {
-                $query->whereRaw('price ' . $data['operator'] . ' ?', $data['price_start']);
-            }
-        }
-    })->get();
+    $data['realestates_details'] = $query->paginate(5);
 
-
-    $data['realestates_details'] = $query;
     return view('realestates_details.index', $data);
 }
-
-
 
 }
--- resources/views/realestates_details/index.blade.php
+++ resources/views/realestates_details/index.blade.php
@@ -8,72 +8,73 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-
-
 </head>
 <body>
-    <div class = "container-fluid" >
+    <div class="container-fluid">
 
-    <h1>Pretraga nekretnina</h1>
-    <div class = "card">
-        <div class = "card-header">
+        <h1>Pretraga nekretnina</h1>
 
-        <form class = "row row-cols-lg-auto g-1" >
-           <div class = "col">
-              <select class = "form-select" name = "location_id">
-                <option value="">Sve lokacije</option>
-                @foreach($locations as $location)
-                <option value="{{ $location }}" {{ $location == $selectedLocation ? 'selected' : '' }}>
-                     {{ $location }}
-              </option>
-                @endforeach
-              </select>
+        <div class="card">
+            <div class="card-header">
+                <form class="row row-cols-lg-auto g-1">
+                    <div class="col">
+                        <select class="form-select" name="location_name">
+                            <option value="">Sve lokacije</option>
+                            @foreach($locations as $location)
+                                @if($selectedLocation == $location)
+                                    <option value="{{ $location }}" selected>{{ $location }}</option>
+                                @else
+                                    <option value="{{ $location }}">{{ $location }}</option>
+                                @endif
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="col">
+                        <input type="text" class="form-control" name="q" value="{{ $q }}" placeholder="Pretrazi..." />
+                    </div>
+                    <div class="input-group">
+                        <select class="form-select" name="operator">
+                            <option value="">Opseg cene</option>
+                            @foreach($operators as $key => $val)
+                                @if($operator == $key)
+                                    <option value="{{ $key }}" selected>{{ $val }}</option>
+                                @else
+                                    <option value="{{ $key }}">{{ $val }}</option>
+                                @endif
+                            @endforeach
+                        </select>
+                        <input type="text" class="form-control" name="price_start" value="{{ $price_start }}" />
+                        <input type="text" class="form-control" name="price_end" value="{{ $price_end }}" />
+                    </div>
+                    <div class="col">
+                        <button class="btn btn-success">Pronadji</button>
+                    </div>
+                </form>
             </div>
-            <div class = "col">
-                <input type="text" class = "form-control" name = "q" value = "{{ $q }}" placeholder = "Pretrazi..." />
-            </div>
-            <div class = "input-group">
-                 <select class = "form-select" name = "operators">
-                   <option value="">Opseg</option>
-                    @foreach($operators as $key => $val)
-                    @if($operator == $key)
-                       <option value="{{ $key }}" selected >{{ $val }}</option>
-                    @else
-                    <option value="{{ $key }}">{{ $val }}</option>
-                    @endif
-                     @endforeach
-                 </select>
-                 <input type="text" class = "form-control" name = "price_start" value = "{{ $price_start }}" />
-                 <input type="text" class = "form-control" name = "price_end" value = "{{ $price_end }}"  />
-            </div>
-            <div class = "col">
-                <button class = "btn btn-success">Osvezi</button>
-            </div>
-        </form>
+            <div class="card-body p-0">
+                <table class="table table-hover table-bordered table-striped table sm m-0">
+                    <thead>
+                        <th>Username</th>
+                        <th>Lokacija</th>
+                        <th>Opis</th>
+                        <th>Cena nekretnine</th>
+                        <th>Broj pratilaca</th>
+                    </thead>
 
+                    @foreach($realestates_details as $realestate)
+                        <tr>
+                            <td>{{ $realestate->username }}</td>
+                            <td>{{ $realestate->location }}</td>
+                            <td>{{ $realestate->description }}</td>
+                            <td>{{ number_format($realestate->price, 2) }}</td>
+                            <td>{{ $realestate->followers_count }}</td>
+                        </tr>
+                    @endforeach
+                </table>
+
+                {{ $realestates_details->links() }}
+            </div>
         </div>
-        <div class = "card-body p-0">
-            <table class = "table table-hover table-bordered table-striped table sm m-0">
-                <thead>        
-                    <th>Username</th>
-                    <th>Lokacija</th>
-                    <th>Opis</th>
-                    <th>Cena nekretnine</th>
-                    <th>Broj pratilaca</th>
-                </thead>
-
-                @foreach($realestates_details as $realestates_details)
-                  <tr>     
-                    <td>{{ $realestates_details -> username  }}</td>
-                    <td>{{ $realestates_details -> location  }}</td>
-                    <td>{{ $realestates_details -> description  }}</td>
-                    <td>{{ number_format($realestates_details -> price, 2)  }}</td>
-                    <td>{{ $realestates_details -> followers_count  }}</td>
-                </tr>
-                @endforeach
-            </table>
-        </div>
-    </div>
 
     </div>
 </body>
